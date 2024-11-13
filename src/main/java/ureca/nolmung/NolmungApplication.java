970b758dfa4a4150b5c047a4c653b2dfbf116815
dhfkdlsj <dhfkdlsj1@gmail.com>
2024-11-13 17:42:44 +0900

    init: init nolmung project

---
+++ src/main/java/ureca/nolmung/NolmungApplication.java
@@ -0,0 +1,13 @@
+package ureca.nolmung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NolmungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NolmungApplication.class, args);
+    }
+
+}
