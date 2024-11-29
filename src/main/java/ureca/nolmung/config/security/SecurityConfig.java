23a68afaff3a5cde30b70f9b6cee5f6c147d996b
dhfkdlsj <dhfkdlsj1@gmail.com>
2024-11-29 18:17:55 +0900

    env/SV-21: 시큐리티 전체 허용

--- src/main/java/ureca/nolmung/config/security/SecurityConfig.java
+++ src/main/java/ureca/nolmung/config/security/SecurityConfig.java
@@ -40,7 +40,8 @@ public class SecurityConfig {
                 @Override
                 public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                     CorsConfiguration config = new CorsConfiguration();
-                    config.setAllowedOrigins(Arrays.asList("http://localhost:8080", "https://nolmung.org", "http://localhost:3000", "https://develop--nolmung.netlify.app"));
+//                    config.setAllowedOrigins(Arrays.asList("http://localhost:8080", "https://nolmung.org", "http://localhost:3000", "https://develop--nolmung.netlify.app"));
+                    config.setAllowedOrigins(Arrays.asList("*"));
                     config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                     config.setAllowCredentials(true);
                     config.setAllowedHeaders(Arrays.asList("Cache-Control", "Content-Type", "X-Api-Key"));
