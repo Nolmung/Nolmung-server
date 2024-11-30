7bd5b4091ebecd46d3c3a7f15cff1b6cd3a5dc23
고경남 <rhrudska987@naver.com>
2024-11-30 09:43:54 +0900

    refactor/SV-203: 스웨거 지도에서 장소 검색 API 오류 해결

--- src/main/java/ureca/nolmung/config/swagger/SwaggerConfig.java
+++ src/main/java/ureca/nolmung/config/swagger/SwaggerConfig.java
@@ -2,7 +2,6 @@ package ureca.nolmung.config.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
@@ -11,7 +10,6 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swaggy.swagger.customlib.config.SwaggyConfig;
 
 @OpenAPIDefinition(
 	servers = {
@@ -20,7 +18,6 @@ import io.swaggy.swagger.customlib.config.SwaggyConfig;
 	}
 )
 @Configuration
-@Import(SwaggyConfig.class)
 public class SwaggerConfig {
 
 	@Bean
