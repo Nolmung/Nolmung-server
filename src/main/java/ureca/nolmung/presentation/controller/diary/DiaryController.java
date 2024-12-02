1dd1e2eaefbea1f3ff8bd7f4b9d0680d5d4863f5
hyewonbae <bhwn98@gmail.com>
2024-12-02 09:51:38 +0900

    feat/SV-209: Swagger 관련 어노테이션 추가

--- src/main/java/ureca/nolmung/presentation/controller/diary/DiaryController.java
+++ src/main/java/ureca/nolmung/presentation/controller/diary/DiaryController.java
@@ -1,5 +1,7 @@
 package ureca.nolmung.presentation.controller.diary;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import ureca.nolmung.business.diary.DiaryUseCase;
@@ -9,6 +11,7 @@ import ureca.nolmung.business.diary.dto.response.*;
 import ureca.nolmung.config.response.ResponseDto;
 import ureca.nolmung.config.response.ResponseUtil;
 
+@Tag(name = "일기")
 @RestController
 @RequestMapping("/api/v1/diary")
 @RequiredArgsConstructor
@@ -16,26 +19,31 @@ public class DiaryController {
 
     private final DiaryUseCase diaryUseCase;
 
+    @Operation(summary = "일기 등록")
     @PostMapping("")
     public ResponseDto<AddDiaryResp> addDiary(@RequestBody AddDiaryReq req) {
         return ResponseUtil.SUCCESS("일기 생성에 성공하였습니다.", diaryUseCase.addDiary(req.userId(), req));
     }
 
+    @Operation(summary = "일기 목록 조회")
     @GetMapping("")
     public ResponseDto<DiaryListResp> getAllDiaries(@RequestParam Long userId) {
         return ResponseUtil.SUCCESS("일기 조회에 성공하였습니다.", diaryUseCase.getAllDiaries(userId));
     }
 
+    @Operation(summary = "일기 상세 조회")
     @GetMapping("/{diaryId}")
     public ResponseDto<DiaryDetailResp> getDetailDiary(@PathVariable Long diaryId) {
         return ResponseUtil.SUCCESS("일기 상세조회에 성공하였습니다.", diaryUseCase.getDetailDiary(diaryId));
     }
 
+    @Operation(summary = "일기 삭제")
     @DeleteMapping("/{diaryId}")
     public ResponseDto<DeleteDiaryResp> deleteDiary(@PathVariable Long diaryId) {
         return ResponseUtil.SUCCESS("일기 삭제에 성공하였습니다.", diaryUseCase.deleteDiary(diaryId));
     }
 
+    @Operation(summary = "일기 수정")
     @PatchMapping("/{diaryId}")
     public ResponseDto<UpdateDiaryResp> updateDiary(@PathVariable Long diaryId, @RequestBody UpdateDiaryReq req) {
         return ResponseUtil.SUCCESS("일기 수정에 성공하였습니다.", diaryUseCase.updateDiary(diaryId, req));
--- src/main/java/ureca/nolmung/presentation/controller/review/ReviewController.java
+++ src/main/java/ureca/nolmung/presentation/controller/review/ReviewController.java
@@ -1,5 +1,7 @@
 package ureca.nolmung.presentation.controller.review;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import ureca.nolmung.business.review.ReviewUseCase;
@@ -8,6 +10,7 @@ import ureca.nolmung.business.review.dto.response.AddReviewResp;
 import ureca.nolmung.config.response.ResponseDto;
 import ureca.nolmung.config.response.ResponseUtil;
 
+@Tag(name = "후기")
 @RestController
 @RequestMapping("/api/v1/reviews")
 @RequiredArgsConstructor
@@ -15,23 +18,17 @@ public class ReviewController {
 
     private final ReviewUseCase reviewUseCase;
 
-    /**
-     * 24.11.22 작성자 : 배혜원
-     * 후기 등록
-     * */
+    @Operation(summary = "후기 등록")
     @PostMapping("")
     public ResponseDto<Long> addReview(Long userId, @RequestBody AddReviewReq req) {
         Long createReviewId = reviewUseCase.addReview(userId, req);
-        return ResponseUtil.SUCCESS("리뷰 생성에 성공하였습니다.",createReviewId);
+        return ResponseUtil.SUCCESS("후기 생성에 성공하였습니다.",createReviewId);
     }
 
-    /**
-     * 24.11.22 작성자 : 배혜원
-     * 후기 삭제
-     * */
+    @Operation(summary = "후기 삭제")
     @DeleteMapping("/{reviewId}")
     public ResponseDto<Long> deleteReview(@PathVariable Long reviewId) {
         Long deleteReviewId = reviewUseCase.deleteReview(reviewId);
-        return ResponseUtil.SUCCESS("리뷰 삭제에 성공하였습니다.", deleteReviewId);
+        return ResponseUtil.SUCCESS("후기 삭제에 성공하였습니다.", deleteReviewId);
     }
 }
