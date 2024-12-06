6cd93cb21bd1bfdd03003f4fe65a5313a3d67991
고경남 <rhrudska987@naver.com>
2024-12-06 15:20:16 +0900

    feat(SV-222): 장소 조회 필터링 API 수정

--- src/main/java/ureca/nolmung/business/place/PlaceService.java
+++ src/main/java/ureca/nolmung/business/place/PlaceService.java
@@ -51,8 +51,8 @@ public class PlaceService implements PlaceUseCase {
 	}
 
 	@Override
-	public List<SearchedPlaceResponse> findBySearchOption(User user, Category category, String acceptSize, Double ratingAvg, Boolean isBookmarked, double latitude, double longitude, double maxLatitude, double maxLongitude) {
-		List<Place> places = placeManager.findBySearchOption(user, category, acceptSize, ratingAvg, isBookmarked, latitude, longitude, maxLatitude, maxLongitude);
+	public List<SearchedPlaceResponse> findBySearchOption(User user, Category category, Boolean isVisited, Boolean isBookmarked, double latitude, double longitude, double maxLatitude, double maxLongitude) {
+		List<Place> places = placeManager.findBySearchOption(user, category, isVisited, isBookmarked, latitude, longitude, maxLatitude, maxLongitude);
 		return placeDtoMapper.toSearchedPlaceReponseList(places);
 	}
 
