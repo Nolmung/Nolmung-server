b39373c34ed876dadea6288fd9f73c687f7c6dde
JHJ08 <161445093+JHJ08@users.noreply.github.com>
2024-12-09 21:28:22 -0800

    feat/SV-264 : 404 에러 예외처리 코드 추가

--- src/main/java/ureca/nolmung/config/security/SecurityConfig.java
+++ src/main/java/ureca/nolmung/config/security/SecurityConfig.java
@@ -67,6 +67,7 @@ public class SecurityConfig {
 				.requestMatchers("/v1/places/**").permitAll()
 				.requestMatchers("/v1/diary/public/**").permitAll()
 				.requestMatchers("/actuator/prometheus").permitAll()
+				.requestMatchers("/error").permitAll()
 				.requestMatchers("/").permitAll()
 				.anyRequest().authenticated()  // 그 외 URL은 인증 필요
 			)
