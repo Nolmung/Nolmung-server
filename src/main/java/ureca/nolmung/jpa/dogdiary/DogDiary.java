de6a450233c017fdb2b1f86c821550944425ecda
JHJ08 <161445093+JHJ08@users.noreply.github.com>
2024-12-10 02:13:13 -0800

    fix/SV-269 : 반려견 프로필 삭제 시, 발생하는 오류 수정

--- src/main/java/ureca/nolmung/jpa/dogdiary/DogDiary.java
+++ src/main/java/ureca/nolmung/jpa/dogdiary/DogDiary.java
@@ -1,6 +1,16 @@
 package ureca.nolmung.jpa.dogdiary;
 
-import jakarta.persistence.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -26,5 +36,6 @@ public class DogDiary {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "dog_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private Dog dog;
 }
